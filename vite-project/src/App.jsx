fabe0de8027be936cf1223d377de2f00f331602c
Henrique Ferreira da Silva <00001213449856sp@al.educacao.sp.gov.br>
2025-03-18 19:16:13 -0300

    first commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,41 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import './App.css'
-   
-function QtdeCliques({qtdeCliques, titleCliques}) {
-  return(
-      <div>
-          <p>Você clicou {qtdeCliques} em {titleCliques}</p>
-      </div>
+import './App.css';
+ 
+function QtdeCliques({ qtdeCompra, tiposumo }) {
+  return (
+    <div>
+      <p>Você comprou {tiposumo} {qtdeCompra} vezes</p>
+    </div>
   );
 }
+ 
 function Home() {
-    const [SucoLimao, setSucoLimao] = useState(0)
-
-    function CompraSucoDeLimao(){
-      setSucoLimao(SucoLimao + 1);
-    }
-    return (<div className="card">
-              <h1>Página Inicial</h1>
-              <h2>Clique para comprar suco de limão</h2>
-              <button onClick={CompraSucoDeLimao}>Comprar suco de limao</button>
-              <QtdeCliques qtdeCliques={sucoLimao} titleCliques="suco de limão"/>
-              <Link to="/second-page"><button>Carrinho</button>
-              </Link>
-            </div>
-)}; 
-    
-
-
+  const [qtdeCliques, setQtdeCliques] = useState(0);
+ 
+  function comprarSucoDeLimao() {
+    setQtdeCliques(qtdeCliques + 1);
+  }
+ 
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <h2>Clique para comprar suco de limão</h2>
+      <button onClick={comprarSucoDeLimao}>Comprar suco de limão</button>
+      <QtdeCliques qtdeCompra={qtdeCliques} tiposumo="suco de limão" />
+ 
+      <Link to="/second-page">
+        <button>Carrinho</button>
+      </Link>
+    </div>
+  );
+}
+ 
+function SecondPage() {
+  return <h1>Segunda Página - Carrinho</h1>;
+}
+ 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/second-page" element={<SecondPage />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/second-page" element={<SecondPage />} />
+    </Routes>
   );
 }
-
-export default App
+ 
+export default App;
